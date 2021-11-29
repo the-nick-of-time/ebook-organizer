25aff3ffea0fdddf2c76f3226e0ccefb38e2120e
Nick Thurmes <nthurmes@gmail.com>
2021-11-29 00:08:34 -0700

    Read epub metadata

--- ebooks/organize.py
+++ ebooks/organize.py
@@ -1,7 +1,17 @@
 from pathlib import Path
+from collections import namedtuple
+import epub_meta
+
+
+class Info(namedtuple('Info', ['title', 'author', 'series'])):
+    @classmethod
+    def from_epub(cls, file: Path) -> 'Info':
+        data = epub_meta.get_epub_metadata(file, read_cover_image=False, read_toc=False)
+        # checks for non-empty list
+        author = data['authors'] and data['authors'][0]
+        return cls(data['title'], author, None)
 
 
 def crawl(start: Path):
     for file in start.rglob("*.[em][po][ub][bi]"):
         yield file
-
--- tests/test_organize.py
+++ tests/test_organize.py
@@ -1,12 +1,18 @@
 import unittest
 from pathlib import Path
-from ebooks.organize import crawl
+from ebooks.organize import crawl, Info
 
 
 class TestOrganize(unittest.TestCase):
     def test_crawl(self):
         files = list(crawl(Path('./data')))
         self.assertEqual(len(files), 8)
+
+    def test_read_epub(self):
+        file = Path('./data/Angie Sage - The Magykal Papers.epub')
+        info = Info.from_epub(file)
+        self.assertEqual(info.title, 'The Magykal Papers')
+        self.assertEqual(info.author, 'Angie Sage')
 
 
 if __name__ == '__main__':
