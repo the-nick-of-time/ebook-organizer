4101b37da0902487a933cfd1bd51a371a942ab15
Nick Thurmes <nthurmes@gmail.com>
2021-11-29 00:36:37 -0700

    Give up on series for now
    using epub_meta's method to extract a field named 'series' isn't effective

--- tests/test_organize.py
+++ tests/test_organize.py
@@ -6,13 +6,13 @@ from ebooks.organize import crawl, Info
 class TestOrganize(unittest.TestCase):
     def test_crawl(self):
         files = list(crawl(Path('./data')))
-        self.assertEqual(len(files), 8)
+        self.assertEqual(len(files), 3)
 
     def test_read_epub(self):
-        file = Path('./data/Angie Sage - The Magykal Papers.epub')
+        file = Path('./data/Septimus Heap/Magyk - Angie Sage.epub')
         info = Info.from_epub(file)
-        self.assertEqual(info.title, 'The Magykal Papers')
-        self.assertEqual(info.author, 'Angie Sage')
+        self.assertEqual('Magyk', info.title)
+        self.assertEqual('Angie Sage', info.author)
 
 
 if __name__ == '__main__':
